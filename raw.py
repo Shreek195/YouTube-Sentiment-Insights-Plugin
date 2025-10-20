8ca3b3bd3e57e026d7ac2255915a62a166b28acb
Shree <shreekoshti199@gmail.com>
2025-10-20 18:11:53 +0530

    CICD run - 2

--- raw.py
+++ raw.py
@@ -1,1 +1,6 @@
-from dotenv import load_dotenv
+import unittest
+import mlflow
+import os
+import pandas as pd
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import pickle

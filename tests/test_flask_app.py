659c96de48c4d66ef79085446014cbc3c34184d4
Shree <shreekoshti199@gmail.com>
2025-10-20 19:35:28 +0530

    CICD run - 2 Fixes

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -1,10 +1,10 @@
 import unittest
-from flask_api.main import create_app
+from flask_api.main import app  # import the Flask app instance directly
 
 class FlaskAppTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.client = create_app.test_client()
+        cls.client = app.test_client()  # directly use app.test_client()
 
     def test_home_page(self):
         response = self.client.get('/')
@@ -12,16 +12,13 @@ class FlaskAppTests(unittest.TestCase):
         self.assertIn(b'Welcome to the YouTube Sentiment Insights Flask API', response.data)
 
     def test_predict_page(self):
-        # Send JSON payload with comments
         response = self.client.post('/predict', json={'comments': ['I love this!']})
         self.assertEqual(response.status_code, 200)
 
-        # Parse JSON response
         json_data = response.get_json()
         self.assertIsInstance(json_data, list)
         self.assertGreater(len(json_data), 0)
 
-        # Check that sentiment is one of the expected numeric values
         for item in json_data:
             self.assertIn(item['sentiment'], [-1, 0, 1], "Sentiment should be -1, 0, or 1")
 
